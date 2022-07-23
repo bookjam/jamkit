bf3cc415d5bcea9a1ca70282cb4e33a6ed95f645
Hanyeol Cho <hanyeol.cho@gmail.com>
2022-07-23 20:40:35 +0900

    Refactor command.js

--- commands.js
+++ commands.js
@@ -1,26 +1,25 @@
-const fs             = require('fs-extra'),
-      path           = require('path'),
-      zipdir         = require('zip-dir'),
-      tmp            = require('tmp'),
-      { create: ipfs,
-        globSource } = require('ipfs-http-client'),
-      urlencode      = require('urlencode'),
-      { v4: uuidv4 } = require('uuid'),
-      template       = require('./template'),
-      catalog        = require('./catalog'),
-      simulator      = require('./simulator'),
-      shell          = require('./shell'),
-      syncfolder     = require('./syncfolder'),
-      installer      = require('./installer'),
-      bon            = require('./bon'),
-      leafly         = require('./leafly'),
-      utils          = require('./utils');
+const fs         = require('fs-extra'),
+      path       = require('path'),
+      zipdir     = require('zip-dir'),
+      tmp        = require('tmp'),
+      ipfs       = require('ipfs-http-client'),
+      urlencode  = require('urlencode'),
+      uuid       = require('uuid'),
+      template   = require('./template'),
+      catalog    = require('./catalog'),
+      simulator  = require('./simulator'),
+      shell      = require('./shell'),
+      syncfolder = require('./syncfolder'),
+      installer  = require('./installer'),
+      bon        = require('./bon'),
+      leafly     = require('./leafly'),
+      utils      = require('./utils');
 
 const connect_base_url = "https://jamkit.io";
 
 function _generate_app_id(wanted_app_id, template_app_id) {
     if (wanted_app_id === 'auto') {
-        return 'com.yourdomain.' + uuidv4();
+        return 'com.yourdomain.' + uuid.v4();
     }
 
     if (wanted_app_id === 'manual') {
@@ -125,7 +124,7 @@ function _publish_image(options, ipfs_options, callback) {
 }
 
 async function _publish_file(path, options, callback) {
-    for await (const file of ipfs(options).addAll(globSource('./', path))) {
+    for await (const file of ipfs.create(options).addAll(ipfs.globSource('./', path))) {
         callback(file["cid"]);
     }
 }
