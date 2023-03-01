9a385b24f7e99025d4796bc16669fcd08b8bab94
Chan Ryu <upriser@gmail.com>
2023-03-01 11:12:50 +1100

    Cleanup non-JS debugging messages and code (#16)

--- src/debug/remotedebug/protocols/ios9.ts
+++ src/debug/remotedebug/protocols/ios9.ts
@@ -10,16 +10,4 @@ export class IOS9Protocol extends IOSProtocol {
     constructor(target: Target) {
         super(target);
     }
-
-    protected mapSelectorList(selectorList): void {
-        const range = selectorList.range;
-
-        for (let i = 0; i < selectorList.selectors.length; i++) {
-            if (range !== undefined) {
-                selectorList.selectors[i].range = range;
-            }
-        }
-
-        delete selectorList.range;
-    }
 }
