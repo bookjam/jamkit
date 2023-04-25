bb579a9d4260950df261bab1b322ff5575a43fbf
Chan Ryu <upriser@gmail.com>
2023-04-26 08:58:22 +1000

    update message / comments

--- src/commands.js
+++ src/commands.js
@@ -63,21 +63,22 @@ function _json_stringify(json) {
 }
 
 function _get_vscode_launch_json_path() {
-    // In case this app/book is a part of a collection such as jamkit-recipes,
-    // we would have .vscode in the containing directory.
+    // If this app is a part of an app collection such as jamkit-recipes,
+    // we could have `.vscode` in the parent directory.
     if (fs.pathExistsSync('../.vscode')) {
         return '../.vscode/launch.json';
     }
 
-    // Otherwise, use the app/book (i.e., where package.bon exists) directory.
+    // Otherwise, use the app's project directory, i.e., where the `package.bon` exists.
     return './.vscode/launch.json';
 }
 
 function _write_vscode_launch_json(debugger_port) {
     const json_path = _get_vscode_launch_json_path();
 
+    const CONFIG_NAME = 'Jamkit attach';
     const jamkitAttachConfig = {
-        name: 'Jamkit attach',
+        name: CONFIG_NAME,
         type: 'node',
         request: 'attach',
         port: debugger_port
@@ -92,31 +93,32 @@ function _write_vscode_launch_json(debugger_port) {
         }));
     };
 
-    if (fs.existsSync(json_path)) {
-        console.log(`Checking debugger attach configuration in ${json_path}...`);
-        try {
-            const json = JSON.parse(fs.readFileSync(json_path, 'utf-8'));
-            const index = json.configurations.findIndex(c => c.name === 'Jamkit attach');
-            if (index !== -1) {
-                if (json.configurations[index].port === debugger_port) {
-                    console.log('  Skipped - the existing configuration seems fine.');
-                    return;
-                }
-                json.configurations[index].port = debugger_port;
-            } else {
-                json.configurations.push(jamkitAttachConfig);
-            }
-            fs.writeFileSync(json_path, _json_stringify(json, 'utf8'));
-            console.log(`  Done - the configuration is updated.`);
-        } catch (err) {
-            // it seems that the existing json file is malformed.
-            rewriteLaunchJson();
-            console.log(`  Done - the configuration is re-written.`);
-        }
-    } else {
+    if (!fs.existsSync(json_path)) {
         fs.ensureFileSync(json_path);
         rewriteLaunchJson();
-        console.log(`A debugger attatch configuration is created in ${json_path}.`);
+        console.log(`Created the debugger attatch configuration in ${json_path}.`);
+        return;
+    }
+
+    console.log(`Verifying the exisiting debugger configuration in ${json_path}...`);
+    try {
+        const json = JSON.parse(fs.readFileSync(json_path, 'utf-8'));
+        const index = json.configurations.findIndex(c => c.name === CONFIG_NAME);
+        if (index !== -1) {
+            if (json.configurations[index].port === debugger_port) {
+                console.log('  Done - the existing configuration seems fine.');
+                return;
+            }
+            json.configurations[index].port = debugger_port;
+        } else {
+            json.configurations.push(jamkitAttachConfig);
+        }
+        fs.writeFileSync(json_path, _json_stringify(json, 'utf8'));
+        console.log(`  Done - updated the existing configuration.`);
+    } catch (err) {
+        // the existing launch.json file might have been corrupted.
+        rewriteLaunchJson();
+        console.log(`  Done - rewrote the whole configuration file.`);
     }
 }
 
