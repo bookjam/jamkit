f04ea8dc0511c603fe25cae1fe02483d1a0ac446
Hanyeol Cho <hanyeol.cho@gmail.com>
2025-08-09 18:12:11 +0900

    Update leafly module

--- src/leafly.js
+++ src/leafly.js
@@ -1,26 +1,27 @@
-const fetch = require("node-fetch");
+const axios = require("axios");
 
 module.exports = {
     create_short_url: function(original_url, expiry_date) {
         const url = "https://us-central1-leafly-service.cloudfunctions.net/createShortUrl";
         const params = {
-            "url":  original_url,
+            "url": original_url,
             "expiry-date": expiry_date || 0
-        }
-            
-        return fetch(url, {
-            "method": "POST",
-            "headers":  {
+        };
+
+        return axios.post(url, params, {
+            headers: {
                 "Content-Type": "application/json"
-            },
-            "body":  JSON.stringify(params)
+            }
         })
             .then((response) => {
-                if (response.ok) {
-                    return response.json();
+                return response.data;
+            })
+            .catch((error) => {
+                if (error.response) {
+                    return Promise.reject({ status: error.response.status });
                 } else {
-                    return Promise.reject({ "status": response.status });
+                    return Promise.reject({ status: 500, error: error.message });
                 }
-            });
-    } 
+        });
+    }
 }
