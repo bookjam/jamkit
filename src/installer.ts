8314d0ff75f58d157039474624045cf5b62b657f
Hanyeol Cho <hanyeol.cho@gmail.com>
2025-09-30 16:13:45 +0900

    Improve code quality

--- src/installer.ts
+++ src/installer.ts
@@ -3,28 +3,46 @@ import avdctl from "./avdctl-helper.js";
 
 type Platform = "ios" | "android";
 
-interface PlatformImplementation {
-    install(file: string): void;
+abstract class Installer {
+    abstract install(file: string): void;
 }
 
-const _impl: Record<Platform, PlatformImplementation> = {
-    "ios": {
-        install(file: string): void {
-            // iOS installation implementation (currently empty)
-        }
-    },
-
-    "android": {
-        install(file: string): void {
-            const tmpRoot = "/data/local/tmp";
-            const tmpPath = `${tmpRoot}/${path.basename(file)}`;
-
-            avdctl.shell(`rm -f ${tmpPath}`);
-            avdctl.push(file, tmpRoot);
-            avdctl.intent("android.intent.action.VIEW", `file://${tmpPath}`);
-        }
+class IOSInstaller extends Installer {
+    install(_file: string): void {
+        // iOS installation implementation (currently empty)
     }
-};
+}
+
+class AndroidInstaller extends Installer {
+    install(file: string): void {
+        const tmpRoot = "/data/local/tmp";
+        const tmpPath = `${tmpRoot}/${path.basename(file)}`;
+
+        avdctl.shell(`rm -f ${tmpPath}`);
+        avdctl.push(file, tmpRoot);
+        avdctl.intent("android.intent.action.VIEW", `file://${tmpPath}`);
+    }
+}
+
+class InstallerFactory {
+    private static instances: Map<Platform, Installer> = new Map();
+
+    static create(platform: Platform): Installer {
+        if (!this.instances.has(platform)) {
+            switch (platform) {
+                case "ios":
+                    this.instances.set(platform, new IOSInstaller());
+                    break;
+
+                case "android":
+                    this.instances.set(platform, new AndroidInstaller());
+                    break;
+            }
+        }
+
+        return this.instances.get(platform)!;
+    }
+}
 
 interface InstallerModule {
     install(platform: Platform, file: string): void;
@@ -32,7 +50,8 @@ interface InstallerModule {
 
 const installer: InstallerModule = {
     install(platform: Platform, file: string): void {
-        _impl[platform].install(file);
+        const installerImpl = InstallerFactory.create(platform);
+        installerImpl.install(file);
     }
 };
 
